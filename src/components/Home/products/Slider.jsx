916b47a298e321feef8102ea949fad3da83c0ca5
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-12-19 17:54:18 +0200

    links on homepage done

--- src/components/Home/products/Slider.jsx
+++ src/components/Home/products/Slider.jsx
@@ -9,207 +9,208 @@ import brand7 from '../../../images/brand/test/brand7.jpg';
 import brand18 from '../../../images/brand/test/brand18.jpg';
 import brand1 from '../../../images/brand/test/brand1.jpg';
 import '../../../styles/home-styles/products.css';
-import Flicking from "@egjs/react-flicking";
+import Flicking from '@egjs/react-flicking';
+import { Link } from 'react-router-dom';
+
 function Slider() {
   return (
     <>
       <section className='multi-slider'>
         <div className='lamar-container'>
-        <Flicking circular={true} >
-       
-              <div className='box' >
-                <div className='slide-img'>
-                  <img src={brand13} alt='' />
+          <Flicking circular={true}>
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand13} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+                <div className='overlay'>
+                  <Link
+                    to='/Abaya'
+                    onClick={() => {
+                      window.scrollTo({
+                        left: 0,
+                        top: 0,
+                        behavior: 'smooth',
+                      });
+                    }}
+                  >
+                    <a className='buy-btn'>buy now</a>
+                  </Link>
+                </div>
+              </div>
+
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand12} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-        
-         
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand12} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand7} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-          
-    
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand7} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href=''>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href=''>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand1} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-         
-     
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand1} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>neo</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>neo</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand14} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-       
- 
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand14} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand11} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-       
-     
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand11} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand2} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-         
-        
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand2} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>lamar</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>lamar</span>
-                  </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
+              </div>
+            </div>
 
-                  <a href='#' className='price'>
-                    QAR 1,300
+            <div className='box'>
+              <div className='slide-img'>
+                <img src={brand18} alt='' />
+
+                <div className='overlay'>
+                  <a href='#' className='buy-btn'>
+                    buy now
                   </a>
                 </div>
               </div>
-       
-            
-              <div className='box'>
-                <div className='slide-img'>
-                  <img src={brand18} alt='' />
 
-                  <div className='overlay'>
-                    <a href='#' className='buy-btn'>
-                      buy now
-                    </a>
-                  </div>
+              <div className='details'>
+                <div className='type'>
+                  <a href='#'>lolo fashion</a>
+                  <span>neo</span>
                 </div>
 
-                <div className='details'>
-                  <div className='type'>
-                    <a href='#'>lolo fashion</a>
-                    <span>neo</span>
-                  </div>
-
-                  <a href='#' className='price'>
-                    QAR 1,300
-                  </a>
-                </div>
+                <a href='#' className='price'>
+                  QAR 1,300
+                </a>
               </div>
-      
-        </Flicking>
-         
+            </div>
+          </Flicking>
         </div>
       </section>
     </>
